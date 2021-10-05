e0df4cea61709789ec2bcbc054f19696eb730dbd
Mehmet Ali Baykara <mehmetalibaykara@gmail.com>
2021-10-05 20:39:57 +0200

    read

---
+++ script.sh
@@ -0,0 +1,13 @@
+#!/bin/env bash
+
+mvn clean package
+
+echo "Your docker username"
+
+docker build -t baykara/springcloud .
+echo "Image is pushing to registry..."
+docker push baykara/springcloud
+
+
+echo "For Deploying to Kubernetes Cluster"
+echo "replace "
--- src/main/java/com/springcloud/example/MyBean.java
+++ src/main/java/com/springcloud/example/MyBean.java
@@ -3,17 +3,25 @@ package com.springcloud.example;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
 
 @Component
+// @ConfigurationProperties()
+@PropertySource("test.yml")
 public class MyBean {
 
-	@Autowired
-	private PropertiesConfig myConfig;
+	@Value("${name}")
+	private String name;
 
 	@Scheduled(fixedDelay = 5000)
 	public void hello() {
-		System.out.println("The first message is: " + this.myConfig.getSurname());
-        System.out.println("The second message is: " + this.myConfig.getName());
+		System.out.println("The first message is: " + this.name);
 	}
 
 }
