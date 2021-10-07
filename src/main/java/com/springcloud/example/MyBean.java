77e8cc5525074414c1dc14b39263c84bf42f241b
FoxNeo <m.caceres@mailbox.org>
2021-10-08 00:20:43 +0200

    Add some optimizations and improvements in pom.xml

--- src/main/java/com/springcloud/example/MyBean.java
+++ src/main/java/com/springcloud/example/MyBean.java
@@ -1,23 +1,17 @@
 package com.springcloud.example;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import lombok.Data;
 
 @Component
 @ConfigurationProperties()
 @Data
 public class MyBean {
 
-	@Value("${spring.application.name}")
+	@Value("${spring.application.name:default-value}")
 	private String name;
 
 	@Scheduled(fixedDelay = 2000)
