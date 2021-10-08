7cfea921d55572290bf159d3c4e09238484c22a1
Mehmet Baykara <mehmet.baykara@teamviewer.com>
2021-10-08 10:26:57 +0200

    add missing import

--- src/main/java/com/springcloud/example/MyBean.java
+++ src/main/java/com/springcloud/example/MyBean.java
@@ -4,6 +4,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import lombok.Data;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Configuration;
+
+
 
 @Data
 @Configuration
