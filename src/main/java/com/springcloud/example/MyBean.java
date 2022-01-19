5974fdae7f84c58bcb054429bc8ff7a39c521c41
Mehmet Baykara <mehmet.baykara@teamviewer.com>
2022-01-19 21:44:42 +0100

    wip

--- src/main/java/com/springcloud/example/MyBean.java
+++ src/main/java/com/springcloud/example/MyBean.java
@@ -4,12 +4,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import lombok.Data;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.context.annotation.Configuration;
-
 
 
 @Data
-@Configuration
 @RefreshScope
 public class MyBean {
 
--- src/main/java/com/springcloud/example/RuntimeController.java
+++ src/main/java/com/springcloud/example/RuntimeController.java
@@ -4,7 +4,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
