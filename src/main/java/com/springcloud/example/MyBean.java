f341fa3dcf680fbd2fefa14c53caa307fe414d76
Mehmet Baykara <mehmet.baykara@teamviewer.com>
2022-01-20 16:36:22 +0100

    v1

--- src/main/java/com/springcloud/example/MyBean.java
+++ src/main/java/com/springcloud/example/MyBean.java
@@ -1,16 +1,17 @@
 package com.springcloud.example;
 
+import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
 // import org.springframework.boot.context.properties.ConfigurationProperties;
-import lombok.Data;
-// import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+
+// import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
-
-
 @Data
+// @ConfigurationProperties
+@RefreshScope
 @Configuration
-// @RefreshScope
 public class MyBean {
 
 	@Value("${spring.application.name: default}")
